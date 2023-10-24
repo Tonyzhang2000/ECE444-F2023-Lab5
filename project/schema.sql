bd3594191627c810e91031c313299d94c7f08575
Tonyzhang2000 <zhangshutong2000@gmail.com>
2023-10-24 12:15:05 -0400

    finish search

--- project/schema.sql
+++ project/schema.sql
@@ -1,6 +1,12 @@
-drop table if exists entries;
+drop table if exists entries post;
 
 create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  text text not null
+);
+
+create table post (
   id integer primary key autoincrement,
   title text not null,
   text text not null
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ import json
 import pytest
 from pathlib import Path
 
-from project.app import app, init_db
+from project.app import app, db
 
 TEST_DB = "test.db"
 
@@ -13,10 +13,12 @@ def client():
     BASE_DIR = Path(__file__).resolve().parent.parent
     app.config["TESTING"] = True
     app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{BASE_DIR.joinpath(TEST_DB)}"
 
-    init_db() # setup
-    yield app.test_client() # tests run here
-    init_db() # teardown
+    with app.app_context():
+        db.create_all()  # setup
+        yield app.test_client()  # tests run here
+        db.drop_all()  # teardown
 
 
 def login(client, username, password):
@@ -25,6 +27,13 @@ def login(client, username, password):
         "/login",
         data=dict(username=username, password=password),
         follow_redirects=True,
+    )
+
+def search(client):
+    """Search helper function"""
+    return client.get(
+        "/search",
+        follow_redirects=True
     )
 
 
@@ -60,6 +69,11 @@ def test_login_logout(client):
     assert b"Invalid username" in rv.data
     rv = login(client, app.config["USERNAME"], app.config["PASSWORD"] + "x")
     assert b"Invalid password" in rv.data
+
+def test_searh(client):
+    """Test search"""
+    rv = search(client)
+    assert b"Search:" in rv.data
 
 
 def test_messages(client):
